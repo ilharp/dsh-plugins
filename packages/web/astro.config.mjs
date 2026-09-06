bd7cd393180d64ab88e06725490e1be8749f34b0
Il Harper <hi@ilharper.com>
2026-09-07 01:51:09 +0800

    feat: update astro config

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,7 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import mdx from '@astrojs/mdx'
+import node from '@astrojs/node'
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  integrations: [react()]
-});
+  site: 'https://dsh.ilharper.com',
+  trailingSlash: 'never',
+  adapter: node({ mode: 'standalone' }),
+  integrations: [mdx(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
